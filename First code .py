4c3f94c47d9e34e60d4bfe97a7a7386c4211c967
Nadim660 <100480279+Nadim660@users.noreply.github.com>
2022-10-05 01:59:29 +0600

    Bal chal

---
+++ First code .py
@@ -0,0 +1,29 @@
+#name = "rahim mim"
+
+'''n=10
+print(""+name)
+print("",n)'''
+
+
+'''name = input("enter your name: ")
+
+age = input("Enter age : ")
+
+gpa = input("Enter GPA : ")
+
+
+print("Student information !!!")
+print("Name : "+name)
+print("AGE : "+age)
+print("GPA : "+gpa)'''
+
+a = 18 
+b = 4
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
---
+++ second prog.py
@@ -0,0 +1,29 @@
+from imaplib import Int2AP
+from math import *
+
+'''num = float(input("enter number = "))
+
+num2 = float(input("enter second number = "))
+
+area = .5*num*num2
+print(area)'''
+
+#m = int(input("enter values : "))
+
+#result = max(20,10)
+
+#print("sum",result)
+
+#print(max(m))
+
+#print(min(10,20))
+
+#num = 11
+
+#print(type(num))
+
+num1= int(input())
+num2 = int(input())
+
+print(f"{num1} + {num2} = {num1+num2}")
+
